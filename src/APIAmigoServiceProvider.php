67b8f37ec22af76103433915bc02413fa2b2dab5
chrisreedio <chrisreedio@users.noreply.github.com>
2023-12-16 07:40:15 +0000

    Fix styling

--- src/APIAmigoServiceProvider.php
+++ src/APIAmigoServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace ChrisReedIO\APIAmigo;
 
+use ChrisReedIO\APIAmigo\Commands\APIAmigoCommand;
+use ChrisReedIO\APIAmigo\Testing\TestsAPIAmigo;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use ChrisReedIO\APIAmigo\Commands\APIAmigoCommand;
-use ChrisReedIO\APIAmigo\Testing\TestsAPIAmigo;
 
 class APIAmigoServiceProvider extends PackageServiceProvider
 {
