939cb15351b77fb0684f3b2b0822d64de529a6e4
chrisreedio <chrisreedio@users.noreply.github.com>
2024-01-22 23:12:11 +0000

    Fix styling

--- src/Resources/AmigoWebhookResource/Pages/ListAmigoWebhooks.php
+++ src/Resources/AmigoWebhookResource/Pages/ListAmigoWebhooks.php
@@ -11,6 +11,7 @@ class ListAmigoWebhooks extends ListRecords
     protected static string $resource = AmigoWebhookResource::class;
 
     protected ?string $maxContentWidth = 'full';
+
     protected function getHeaderActions(): array
     {
         return [
