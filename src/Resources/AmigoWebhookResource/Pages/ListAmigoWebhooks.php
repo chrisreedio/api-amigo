08f33a295b0a64e4d1b323f0ee75fbac8e10515a
Chris Reed <chris@reedtech.us>
2024-01-22 17:11:45 -0600

    feat: enhance webhook UI with additional columns
    - Include 'Listener', 'Processed', 'Sender', and 'Sent At' columns to AmigoWebhookResource
    - Add default 'Success' state and color coding to 'Result / Error Message' in AmigoWebhooksRelationManager
    - Set max content width to 'full' for ListAmigoWebhooks page
    
    This update improves the visibility of webhook processing status and errors, simplifies identification of the listener, and enhances the overall user experience by providing more context and actionable information on the webhook interface.

--- src/Resources/AmigoWebhookResource/Pages/ListAmigoWebhooks.php
+++ src/Resources/AmigoWebhookResource/Pages/ListAmigoWebhooks.php
@@ -10,6 +10,7 @@ class ListAmigoWebhooks extends ListRecords
 {
     protected static string $resource = AmigoWebhookResource::class;
 
+    protected ?string $maxContentWidth = 'full';
     protected function getHeaderActions(): array
     {
         return [
