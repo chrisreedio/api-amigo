4ba29017ea898e8ca54e576cc972c5dd1d4fbf2f
chrisreedio <chrisreedio@users.noreply.github.com>
2024-05-14 17:46:55 +0000

    Fix styling

--- src/Resources/AmigoEndpointResource/Widgets/EndpointStatsOverview.php
+++ src/Resources/AmigoEndpointResource/Widgets/EndpointStatsOverview.php
@@ -20,7 +20,7 @@ class EndpointStatsOverview extends BaseWidget
         $total = $this->record->aggregates->sum('total_requests');
 
         // Calculate the average response time -- TODO: Replace with P90 (or one of the other percentiles)
-        $tempAverage = number_format(($this->record->aggregates->average('avg_duration') * 1000 ?? 0)).'ms';
+        $tempAverage = number_format(($this->record->aggregates->average('avg_duration') * 1000 ?? 0)) . 'ms';
 
         // Calculate the success percent from the total and failed counts
         $totalSuccesses = $this->record->aggregates->sum('successful_requests');
@@ -42,7 +42,7 @@ class EndpointStatsOverview extends BaseWidget
             Stat::make('Total Responses', number_format($total)),
             Stat::make('P90 Response Time', $tempAverage),
             Stat::make('Success Rate', $successPercent)
-                ->value(new HtmlString('<span style="color: rgb('.$successColor.')">'.$successPercent.'</span>')),
+                ->value(new HtmlString('<span style="color: rgb(' . $successColor . ')">' . $successPercent . '</span>')),
         ];
     }
 }
