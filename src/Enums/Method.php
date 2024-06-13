3f221e92ba9bbddd7476766e95b55aae8e241770
Chris Reed <chris@reedtech.us>
2024-06-13 02:42:37 -0500

    ✨ feat(`Method` enum & `AmigoEndpoint`): Added HTTP methods and improved method handling
    Expanded the `Method` enum with new HTTP methods and refactored `AmigoEndpoint` to use `Method::from`.

--- src/Enums/Method.php
+++ src/Enums/Method.php
@@ -9,19 +9,27 @@ use Filament\Support\Contracts\HasLabel;
 enum Method: string implements HasColor, HasLabel
 {
     case GET = 'GET';
+    case HEAD = 'HEAD';
     case POST = 'POST';
     case PUT = 'PUT';
-    case DELETE = 'DELETE';
     case PATCH = 'PATCH';
+    case DELETE = 'DELETE';
+    case OPTIONS = 'OPTIONS';
+    case CONNECT = 'CONNECT';
+    case TRACE = 'TRACE';
 
     public function getColor(): string | array | null
     {
         return match ($this) {
             self::GET => Color::Green,
+            self::HEAD => Color::Gray,
             self::POST => Color::Blue,
             self::PUT => Color::Yellow,
-            self::DELETE => Color::Red,
             self::PATCH => Color::Purple,
+            self::DELETE => Color::Red,
+            self::OPTIONS => Color::Indigo,
+            self::CONNECT => Color::Pink,
+            self::TRACE => Color::Teal,
         };
     }
 
